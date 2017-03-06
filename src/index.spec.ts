2c8590e7dab720fb6aebe97b5be751224c5f9fd7
Aitor Oses <aitor.oses@gmail.com>
2017-03-06 10:41:23 +0100

    Added a test for testing recieve/send methods

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -13,3 +13,39 @@ test('erlang process system is available', (t) => {
   t.true(typeof ProcessSystem === 'function')
   t.true(typeof States.NORMAL === 'symbol')
 })
+
+test(function pingPongTest(t) {
+
+  let resolve, promise = new Promise<any>(res => resolve = res)
+  const system = new ProcessSystem()
+
+  const pid1 = system.spawn(function*(){
+    let pid1Receive
+
+    yield system.receive(value => {
+      pid1Receive = value
+    })
+
+    t.true(pid1Receive === 'PING')
+
+    system.send(pid2, 'PONG')
+  })
+
+  system.register('Sally', pid1)
+
+  const pid2 = system.spawn(function*() {
+    let pid2Receive
+
+    system.send('Sally', 'PING')
+
+    yield system.receive(value => {
+      pid2Receive = value
+    })
+
+    t.true(pid2Receive === 'PONG')
+
+    resolve()
+  })
+
+  return promise
+})
